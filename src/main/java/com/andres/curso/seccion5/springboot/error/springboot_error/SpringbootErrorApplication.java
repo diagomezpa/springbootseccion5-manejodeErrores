bdf2842ecbdfdca71c67a65467d63361376da97a
Diagomezpa <Diagomezpa@gmail.com>
2026-03-12 14:17:03 -0500

    iniciando proyecto de errores

---
+++ src/main/java/com/andres/curso/seccion5/springboot/error/springboot_error/SpringbootErrorApplication.java
@@ -0,0 +1,13 @@
+package com.andres.curso.seccion5.springboot.error.springboot_error;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootErrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootErrorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andres/curso/seccion5/springboot/error/springboot_error/SpringbootErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andres.curso.seccion5.springboot.error.springboot_error;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootErrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
