d606f5e6d9e34f19ea5389369459b71abd5a6991
Diagomezpa <Diagomezpa@gmail.com>
2026-03-12 14:27:30 -0500

    seccion 5 78. Creando nueva aplicación y controlador

---
+++ src/main/java/com/andres/curso/seccion5/springboot/error/springboot_error/controllers/AppController.java
@@ -0,0 +1,16 @@
+package com.andres.curso.seccion5.springboot.error.springboot_error.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class AppController {
+
+    @GetMapping("/app")
+    public String index() {
+        return "ok 200";
+    }
+    
+}
